8e1cf4ca1f96dcb1b28a2624476963e5c781debd
Archerfeel <archerfeel@gmail.com>
2017-09-14 17:00:23 +0800

    dagonggaoc

--- awacs-agent/src/main/java/io/awacs/agent/net/PacketAccumulator.java
+++ awacs-agent/src/main/java/io/awacs/agent/net/PacketAccumulator.java
@@ -9,6 +9,8 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -18,7 +20,9 @@ public final class PacketAccumulator implements Configurable {
 
     private static Logger log = Logger.getLogger("AWACS");
 
-    private volatile boolean closed;
+    private volatile boolean closed = false;
+
+    private AtomicInteger bufferNumbers = new AtomicInteger(0);
 
     private Channels channels;
 
@@ -34,27 +38,52 @@ public final class PacketAccumulator implements Configurable {
 
     private int batchLingerMs;
 
-    private int timeout;
-
     @Override
     public void init(Configuration configuration) {
         maxBatchBytes = configuration.getInteger(AWACS.CONFIG_MAX_BATCH_BYTES, AWACS.DEFAULT_MAX_BATCH_BYTES);
         maxBatchNumbers = configuration.getInteger(AWACS.CONFIG_MAX_BATCH_NUMBERS, AWACS.DEFAULT_MAX_BATCH_NUMBERS);
         maxAppendMs = configuration.getInteger(AWACS.CONFIG_MAX_APPEND_MS, AWACS.DEFAULT_MAX_APPEND_MS);
         batchLingerMs = configuration.getInteger(AWACS.CONFIG_BATCH_LINGER_MS, AWACS.DEFAULT_BATCH_LINGER_MS);
-        timeout = configuration.getInteger(AWACS.CONFIG_TIMEOUT_MS, AWACS.DEFAULT_TIMEOUT_MS);
         queue = new ArrayBlockingQueue<>(configuration.getInteger(AWACS.CONFIG_MAX_WAITING_MESSAGE, AWACS.DEFAULT_MAX_WAITING_MESSAGE));
-        channels = new Channels(configuration.getArray(AWACS.CONFIG_SERVER), timeout);
+        channels = new Channels(configuration.getArray(AWACS.CONFIG_SERVER), configuration.getInteger(AWACS.CONFIG_TIMEOUT_MS, AWACS.DEFAULT_TIMEOUT_MS));
         buffers = new ConcurrentLinkedDeque<>();
         start();
     }
 
+    private void writeBatch() {
+        final ByteBuffer buf = buffers.poll();
+        channels.flush(buf, new Callback() {
+            @Override
+            public void onComplete() {
+                buf.clear();
+                buffers.add(buf);
+            }
+
+            @Override
+            public void onException(Throwable t) {
+                buf.clear();
+                buffers.add(buf);
+            }
+        });
+    }
+
+    private boolean allocateNewBuffer(Packet init) {
+        if (bufferNumbers.get() >= maxBatchNumbers) {
+            log.log(Level.WARNING, "Couldn't allocate more buffers.");
+            return false;
+        }
+        ByteBuffer buf = ByteBuffer.allocate(maxBatchBytes);
+        buf.put(init.serialize());
+        buffers.push(buf);
+        return true;
+    }
+
     private void start() {
-        int leastBuffers = 0;
         do {
             buffers.add(ByteBuffer.allocate(maxBatchBytes));
-        } while (leastBuffers++ < maxBatchNumbers / 2);
-        //make sure only this thread can access ByteBuffer
+        } while (bufferNumbers.getAndIncrement() < maxBatchNumbers / 2);
+        log.log(Level.INFO, "{0}x{1} bytes memory allocated.", new Integer[]{maxBatchBytes, bufferNumbers.get()});
+        //make sure only this thread can access buffers
         Thread t = new Thread(new Runnable() {
             long newBatchCreated = 0l;
 
@@ -63,40 +92,31 @@ public final class PacketAccumulator implements Configurable {
                 while (!closed) {
                     try {
                         Packet p = queue.take();
-                        ByteBuffer batch = buffers.peek();
-                        boolean readyToFlush = false;
-                        if (batch != null) {
-                            if (batch.remaining() == batch.capacity() && batch.capacity() >= p.size()) {
+                        if (!buffers.isEmpty()) {
+                            ByteBuffer batch = buffers.peek();
+                            if (batch.remaining() == batch.capacity()) {
                                 batch.put(p.serialize());
                                 newBatchCreated = System.currentTimeMillis();
                             } else if (batch.remaining() >= p.size()) {
                                 batch.put(p.serialize());
                                 if (System.currentTimeMillis() - newBatchCreated > batchLingerMs) {
-                                    readyToFlush = true;
+                                    writeBatch();
                                 }
                             } else {
-                                //TODO
-                                readyToFlush = true;
-                            }
-                            if (readyToFlush) {
-                                final ByteBuffer buf = buffers.poll();
-                                channels.flush(buf, new Callback() {
-                                    @Override
-                                    public void onComplete() {
-                                        buf.clear();
-                                        buffers.add(buf);
-                                    }
-
-                                    @Override
-                                    public void onException(Throwable t) {
-                                        buf.clear();
-                                        buffers.add(buf);
-                                    }
-                                });
+                                writeBatch();
+                                ByteBuffer free = buffers.peek();
+                                if (free != null) {
+                                    free.put(p.serialize());
+                                    newBatchCreated = System.currentTimeMillis();
+                                } else if (allocateNewBuffer(p)) {
+                                    newBatchCreated = System.currentTimeMillis();
+                                }
                             }
                         } else {
-                            //TODO allocating new buffer
-                            log.warning("All buffers are full, allocating a new buffer.");
+                            log.warning("All buffers are full, try to allocate a new buffer.");
+                            if (allocateNewBuffer(p)) {
+                                newBatchCreated = System.currentTimeMillis();
+                            }
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
